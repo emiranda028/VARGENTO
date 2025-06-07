6c86963623b2de180ee68be3cff9d46ae640e30c
Emmanuel Miranda <65192290+emiranda028@users.noreply.github.com>
2025-06-07 16:26:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -158,3 +158,4 @@ if 'df_data' in locals():
     fig_eq_filtrado = px.bar(eq_counts_filtrado, x='Equipo', y='Cantidad', title=f'Jugadas de tipo "{tipo_seleccionado}" por equipo', labels={'Cantidad': 'Cantidad de jugadas'})
     st.plotly_chart(fig_eq_filtrado, use_container_width=True)
 
+   
